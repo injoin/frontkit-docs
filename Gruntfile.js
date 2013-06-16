bb08a2f6fc124f0c353614182ec98a05b49c8526
Gustavo Henke <gustavo@injoin.com.br>
2013-06-16 19:31:38 -0300

    Added copy task to keep styles/scripts/fonts in the docs repo

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,89 @@
 module.exports = function( grunt ) {
-	"use strict";
+    "use strict";
 
-	grunt.initConfig({
-		watch: {
-			submodule: {
-				files: [ "frontkit/styles/*.less", "frontkit/scripts/*.js" ],
-				tasks: [ "hub" ]
-			},
-			docs: {
-				files: [ "templates/**/*.swig", "data/*.json" ],
-				tasks: [ "html" ]
-			},
+    grunt.initConfig({
+        watch: {
+            submodule: {
+                files: [ "frontkit/styles/*.less", "frontkit/scripts/*.js" ],
+                tasks: [ "hub" ]
+            },
+            docs: {
+                files: [ "templates/**/*.swig", "data/*.json" ],
+                tasks: [ "html" ]
+            },
 
-			// Allows us to recompile our docs whenever we change something in mixins/variables files
-			less: {
-				files: [ "assets/**/*.less" ],
-				tasks: [ "less" ]
-			}
-		},
-		hub: {
-			submodule: {
-				src: "frontkit/Gruntfile.js"
-			}
-		},
-		clean: {
-			// Delete previously created HTML files
-			"html-pre": [ "*.html" ]
-		},
-		less: {
-			docs: {
-				files: {
-					"assets/styles/docs.css": "assets/styles/docs.less"
-				}
-			}
-		},
-		swig: {
-			docs: {
-				init: {
-					root: "templates/",
-					tags: require( "./build/swig-tags" )
-				},
-				cwd: "templates/",
-				src: [ "*.swig" ],
-				dest: "./",
-				generateSitemap: false,
-				generateRobotstxt: false,
+            // Allows us to recompile our docs whenever we change something in mixins/variables files
+            less: {
+                files: [ "assets/**/*.less" ],
+                tasks: [ "less" ]
+            }
+        },
+        hub: {
+            submodule: {
+                src: "frontkit/Gruntfile.js"
+            }
+        },
+        clean: {
+            // Delete previously created HTML files
+            "html-pre": [ "*.html" ]
+        },
+        copy: {
+            dist: {
+                files: [
+                    // Styles
+                    {
+                        src: "frontkit/dist/frontkit.css",
+                        dest: "assets/styles/frontkit.css"
+                    },
 
-				// Templates data
-				menus: grunt.file.readJSON( "data/menus.json" )
-			}
-		}
-	});
+                    // Scripts
+                    {
+                        src: "frontkit/dist/frontkit.js",
+                        dest: "assets/scripts/frontkit.js"
+                    },
 
-	grunt.loadNpmTasks("grunt-hub");
-	grunt.loadNpmTasks("grunt-swig");
-	grunt.loadNpmTasks("grunt-contrib-less");
-	grunt.loadNpmTasks("grunt-contrib-clean");
-	grunt.loadNpmTasks("grunt-contrib-watch");
+                    // Fonts
+                    {
+                        src: "frontkit/fonts/*",
+                        dest: "assets/fonts/",
+                        flatten: true,
+                        expand: true
+                    }
+                ]
+            }
+        },
+        less: {
+            docs: {
+                files: {
+                    "assets/styles/docs.css": "assets/styles/docs.less"
+                }
+            }
+        },
+        swig: {
+            docs: {
+                init: {
+                    root: "templates/",
+                    tags: require( "./build/swig-tags" )
+                },
+                cwd: "templates/",
+                src: [ "*.swig" ],
+                dest: "./",
+                generateSitemap: false,
+                generateRobotstxt: false,
 
-	grunt.registerTask( "html", [ "clean:html-pre", "swig" ] );
-	grunt.registerTask( "default", [ "hub", "less", "html" ] );
+                // Templates data
+                menus: grunt.file.readJSON( "data/menus.json" )
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-hub");
+    grunt.loadNpmTasks("grunt-swig");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-less");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+
+    grunt.registerTask( "html", [ "clean:html-pre", "swig" ] );
+    grunt.registerTask( "default", [ "hub", "copy", "less", "html" ] );
 };
