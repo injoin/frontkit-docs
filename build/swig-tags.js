fdef084c59031679dc92320053e789f98a6b9055
Gustavo Henke <gustavo@injoin.com.br>
2013-07-23 19:42:49 -0300

    Fixed bug on multiline code highlights

--- build/swig-tags.js
+++ build/swig-tags.js
@@ -10,7 +10,7 @@ exports.highlight = function() {
     value = hljs.highlight( language, value.trim() ).value;
 
     output += "<pre class=\"highlight language-" + language + "\">";
-    output += value.replace( /\n/g, "\\n" ).split( "'" ).join( "\\'" );
+    output += value.replace( /\r?\n/g, "\\n" ).split( "'" ).join( "\\'" );
     output += "</pre>";
 
     return "_output += '" + output + "';";
